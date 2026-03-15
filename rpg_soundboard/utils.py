f8fea188c1bf947b2ca50746d6f569c62506d002
DiegoJohann <diego9.roberto7@gmail.com>
2026-03-15 08:36:49 -0300

    :sparkles: add support for .opus audio files in file validation

--- rpg_soundboard/utils.py
+++ rpg_soundboard/utils.py
@@ -3,4 +3,4 @@ import pathlib
 
 def is_audio_file(path: str):
     ext = pathlib.Path(path).suffix.lower()
-    return ext in [".mp3", ".ogg", ".wav", ".flac", ".aac", ".m4a"]
+    return ext in [".mp3", ".ogg", ".wav", ".flac", ".aac", ".m4a", ".opus"]
